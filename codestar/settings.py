e73ac418eb57f026f14b2d19f8b523ad032ef30a
MRusinova <rusinovamihaela@gmail.com>
2023-03-29 09:08:28 +0000

    change name of allowed hosts

--- codestar/settings.py
+++ codestar/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['blog-app-mrusinova.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['django-blog-mrusinova.herokuapp.com', 'localhost']
 
 
 # Application definition
